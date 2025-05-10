c1e17d4ad657c8bf0ce3515c34e8e0e5e3ae89fa
invincibleX1 <akrdim12@gmail.com>
2025-05-10 12:25:10 +0300

    Update rain_prediction.ino

--- code/rain_prediction/rain_prediction.ino
+++ code/rain_prediction/rain_prediction.ino
@@ -7,25 +7,17 @@
 #define TFT_CS   5
 #define TFT_DC   17  // A0 pin on TFT
 #define TFT_RST  16
-#define TFT_BL 14
+
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 // BMP280
 Adafruit_BMP280 bmp;
 
-// Button
-#define BUTTON_PIN 4
-
-bool displayOn = false;
-
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   // TFT setup
-  pinMode(TFT_BL, OUTPUT);
-  digitalWrite(TFT_BL, LOW);
   tft.initR(INITR_BLACKTAB);  // adjust as needed
   tft.setRotation(2);
   tft.fillScreen(ST77XX_BLACK);
@@ -39,15 +31,10 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    if (!displayOn) {
-      displayOn = true;  // Track display state
-    }
- 
+  
     float temp = bmp.readTemperature();
     float pressure = bmp.readPressure() / 100.0;
-    digitalWrite(TFT_BL, HIGH);
-    Serial.println("Button Pressed");
+    
 
     tft.fillScreen(ST77XX_BLACK);
     tft.setTextSize(1);
@@ -65,16 +52,7 @@ void loop() {
     tft.setCursor(10, 80);
     tft.print(pressure, 1);
     tft.println(" hPa");
-
-    delay(2000);  // Wait before next reading
-  } else {
-    if (displayOn) {
-      displayOn = false;
-      Serial.println("Button Released - Turning off screen");
-      tft.fillScreen(ST77XX_BLACK);  // Clear screen (simulate off)
-      digitalWrite(TFT_BL, LOW);
-      delay(10);
-    }
-    delay(200);
+  
+    delay(20000);
   }
-}
+
